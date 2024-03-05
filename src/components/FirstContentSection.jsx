eefd29bc2575a8f810b510524544ec024f14cdbd
nsquansah <ninaspioquansah@gmail.com>
2024-03-05 00:38:04 +0000

    app development

--- src/components/FirstContentSection.jsx
+++ src/components/FirstContentSection.jsx
@@ -1,9 +1,17 @@
 import './FirstContentSection.css';
 
 export const FirstContentSection = () => {
-  return (<section className="first-content-section">
-      <h2>First Content Section</h2>
-      <p>Some content</p>
-    </section>
+  return (<div className="first">
+  
+      <h1 className='get'>Get Premium free for 1 month</h1>
+      <p className='para'>Just 119/month after. Debit credit cards accepted. Cancel anytime.</p>
+      
+      <div className='buttons'>
+      <button className='btn'>GET STARTED</button>
+      <button className='btn2'>SEE OTHER PLANS</button>
+      <h3 className='last'><u>Terms and conditions apply.</u> 1 month free not available for users who have already tried Premium.</h3>
+    </div>
+  </div>
+     
   );
 }
